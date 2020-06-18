a4c1a5508458d7c4e849fc0ebc23bb2c6eb2bfc6
Kush Panjabi <kushpanjabi@Kushs-MacBook-Pro.local>
2020-06-18 12:50:28 -0700

    uncomment window location

--- client/src/components/InputBlog.js
+++ client/src/components/InputBlog.js
@@ -18,7 +18,7 @@ const InputBlog = () => {
              });
              
              console.log(response);
-             // window.location = "/";
+             window.location = "/";
          } catch (err) {
              console.error(err.message);
          }
